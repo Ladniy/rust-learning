453c687ac02cd3b4554b437d991547c98040d0ec
Ladniy <ladniy101@gmail.com>
2022-11-04 18:49:14 +0800

    Add release version of project

---
+++ hello_world_cargo/target/release/deps/hello_world_cargo-7ff72fa7e111ba1e.d
@@ -0,0 +1,5 @@
+/Users/ladniy/Git/rust-learning/hello_world_cargo/target/release/deps/hello_world_cargo-7ff72fa7e111ba1e: src/main.rs
+
+/Users/ladniy/Git/rust-learning/hello_world_cargo/target/release/deps/hello_world_cargo-7ff72fa7e111ba1e.d: src/main.rs
+
+src/main.rs:
---
+++ hello_world_cargo/target/release/hello_world_cargo.d
@@ -0,0 +1,1 @@
+/Users/ladniy/Git/rust-learning/hello_world_cargo/target/release/hello_world_cargo: /Users/ladniy/Git/rust-learning/hello_world_cargo/src/main.rs
